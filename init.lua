ac3607380de5e6077fdf0813a7771440d2b83a5e
Dan Caldwell <dan@dans-mbp.lan>
2023-03-01 23:09:26 +0100

    added scnvim stuff

--- init.lua
+++ init.lua
@@ -58,13 +58,20 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+local scnvim = require 'scnvim'
+local map = scnvim.map
+local map_expr = scnvim.map_expr
+
 -- NOTE: Here is where you install your plugins.
 --  You can configure plugins using the `config` key.
 --
 --  You can also configure plugins after the setup call,
 --    as they will be available in your neovim runtime.
+
 require('lazy').setup({
   -- NOTE: First, some plugins that don't require any configuration
+  --
+  'davidgranstrom/scnvim',
 
   -- Git related plugins
   'tpope/vim-fugitive',
@@ -112,11 +119,19 @@ require('lazy').setup({
     },
   },
 
-  { -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
+  -- Theme inspired by Atom
+  --  'navarasu/onedark.nvim',
+  --  priority = 1000,
+  --  config = function()
+  --    vim.cmd.colorscheme 'onedark'
+  --  end,
+  --},
+
+  { -- Theme 
+  'Mofiqul/dracula.nvim',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'onedark'
+      vim.cmd.colorscheme 'dracula'
     end,
   },
 
@@ -191,6 +206,41 @@ require('lazy').setup({
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
+--
+-- vim.g.scnvim_sclang_options = {'-l', "/home/mads/.config/SuperCollider/sclang_conf_development.yaml"}
+
+vim.g.scnvim_postwin_syntax_hl = 1
+
+-- scnvim neovim docs !
+vim.g.scnvim_scdoc = 1
+
+-- vertical 'v' or horizontal 'h' split
+vim.g.scnvim_postwin_orientation = 'v'
+
+-- position of the post window 'right' or 'left'
+vim.g.scnvim_postwin_direction = 'right'
+
+-- default is half the terminal size for vertical and a third for horizontal
+vim.g.scnvim_postwin_size = 50
+
+-- automatically open post window on a SuperCollider error
+vim.g.scnvim_postwin_auto_toggle = 1
+
+--	-- duration of the highlight
+vim.g.scnvim_eval_flash_duration = 100
+
+-- number of flashes. A value of 0 disables this feature.
+vim.g.scnvim_eval_flash_repeats = 2
+
+-- set this variable if you don't want the "echo args" feature
+-- vim.g.scnvim_echo_args = 0
+
+-- Configure the color of the evaluation flash
+vim.cmd([[ highlight SCNvimEval guifg=black guibg=white ctermfg=black ctermbg=white ]])
+
+-- Uncomment to not use default keybindings
+-- vim.g.scnvim_no_mappings = 1
+
 
 -- Set highlight on search
 vim.o.hlsearch = false
@@ -260,6 +310,35 @@ require('telescope').setup {
         ['<C-u>'] = false,
         ['<C-d>'] = false,
       },
+    },
+  },
+}
+
+scnvim.setup {
+  keymaps = {
+    ['<M-e>'] = map('editor.send_line', {'i', 'n'}),
+    ['<C-e>'] = {
+      map('editor.send_block', {'i', 'n'}),
+      map('editor.send_selection', 'x'),
+    },
+    ['<CR>'] = map('postwin.toggle'),
+    ['<M-CR>'] = map('postwin.toggle', 'i'),
+    ['<M-L>'] = map('postwin.clear', {'n', 'i'}),
+    ['<C-k>'] = map('signature.show', {'n', 'i'}),
+    ['<F12>'] = map('sclang.hard_stop', {'n', 'x', 'i'}),
+    ['<leader>st'] = map('sclang.start'),
+    ['<leader>sk'] = map('sclang.recompile'),
+    ['<F1>'] = map_expr('s.boot'),
+    ['<F2>'] = map_expr('s.meter'),
+  },
+  editor = {
+    highlight = {
+      color = 'IncSearch',
+    },
+  },
+  postwin = {
+    float = {
+      enabled = true,
     },
   },
 }
